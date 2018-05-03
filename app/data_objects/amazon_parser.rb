1b5bf4e9e37994c587b4b450b7b0eac5cff23b39
Christopher Dwan <chris@radixhound.com>
2018-05-02 20:03:05 -0700

    Handle other kinds of product page formats

--- app/data_objects/amazon_parser.rb
+++ app/data_objects/amazon_parser.rb
@@ -1,6 +1,19 @@
 class AmazonParser
-  def initialize(body)
-    @doc = Nokogiri::HTML(body)
+  def self.build(body)
+    doc = Nokogiri::HTML(body)
+    if doc.at_xpath("//div[@id = 'prodDetails']")
+      ProductInformationParser.new(doc)
+      # Presumably the page has 'Product information' and we can fetch it that way
+    elsif doc.at_xpath("//div[@id = 'detailBullets']")
+      # Let's try a different parsing method
+      ProductDescriptionParser.new(doc)
+    else
+      NullParser.new
+    end
+  end
+
+  def initialize(html_doc)
+    @doc = html_doc
   end
 
   # Push the data into the DB, overwriting any existing records
@@ -17,6 +30,31 @@ class AmazonParser
     @doc.at_xpath("//span[@id = 'productTitle']").text.gsub(/(\n\s+)+/, '')
   end
 
+  def as_json(options = {})
+    {
+      asin: asin,
+      title: title,
+      rating: rating.to_f,
+      rank: rank.to_i
+    }
+  end
+end
+
+class NullParser
+  def as_json(options = {})
+    {}
+  end
+
+  def present?
+    false
+  end
+
+  def save!
+    raise "You can't save a null Parser"
+  end
+end
+
+class ProductInformationParser < AmazonParser
   def rating
     @doc.at_xpath(product_details_path + customer_reviews_path + star_icon_path).text
   end
@@ -38,13 +76,24 @@ class AmazonParser
   def star_icon_path
     "//i[contains(@class, 'a-icon-star')]"
   end
+end
 
-  def as_json(options = {})
-    {
-      asin: asin,
-      title: title,
-      rating: rating.to_f,
-      rank: rank.to_i
-    }
+class ProductDescriptionParser < AmazonParser
+  def rating
+    @doc.at_xpath(customer_reviews_path + star_icon_path).text
+  end
+
+  # Could also use a sibling selector to get the value based on the contents
+  # of the sibling <td>
+  def rank
+    @doc.at_xpath("//li[@id = 'SalesRank']").text.gsub(/(\n\s+)+/, '').gsub(/^.*?\#/, '')
+  end
+
+  def customer_reviews_path
+    "//div[@id = 'detailBullets_averageCustomerReviews']"
+  end
+
+  def star_icon_path
+    "//i[contains(@class, 'a-icon-star')]"
   end
 end
--- lib/amazon_proxy.rb
+++ lib/amazon_proxy.rb
@@ -1,11 +1,15 @@
-
+#
+# Use rack-proxy to intercept requests that need to go to
+# amazon and process the response, transforming the result
+# into a simple json object and storing the parsed values
+# in the database
+#
 class AmazonProxy < Rack::Proxy
-
   def perform_request(env)
     request = Rack::Request.new(env)
 
     if request.path =~ %r{^/dp}
-      env["HTTP_HOST"] = "www.amazon.com"
+      env['HTTP_HOST'] = 'www.amazon.com'
       @process_response = true
       super(env)
     else
@@ -15,24 +19,27 @@ class AmazonProxy < Rack::Proxy
   end
 
   def rewrite_response(triplet)
-    status, headers, body = triplet
     if @process_response
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = '*'
-
-      if body.first.present?
-        ps = ::AmazonParser.new(body.first)
-        body = ps.to_json
-        ps.save!
-        Rails.logger.info body
-      end
-      [status, headers, Array(body)]
+      scrape_product_page(triplet)
     else
       triplet
     end
   end
+
+  def scrape_product_page(triplet)
+    status, headers, body = triplet
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+
+    if body.first.present?
+      ps = ::AmazonParser.build(body.first)
+      body = ps.to_json
+      Rails.logger.info body
+      ps.save!
+    end
+    [status, headers, Array(body)]
+  end
 end
 
 Rails.application.config.middleware.use AmazonProxy, backend: 'https://www.amazon.com', streaming: false
-
